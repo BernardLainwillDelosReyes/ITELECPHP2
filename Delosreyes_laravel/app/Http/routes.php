96c22c5ef270a4499e2df6df67796aeda3f8edbf
BernardLainwillDelosReyes <32511609+BernardLainwillDelosReyes@users.noreply.github.com>
2017-10-04 18:25:40 +0800

    Add files via upload

---
+++ Delosreyes_laravel/app/Http/routes.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'HomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
+ 
+ Route::get('users',function()
+{
+	return 'Users!';
+}
+);
+
+Route::get('/about',function()
+{
+	return view('about2');
+}
+);
+Route::get('/php2',function()
+{
+	return view('php2');
+}
+);
+Route::get('/subject',function()
+{
+	return view('subject');
+}
+);
+Route::get('/socialmedia',function()
+{
+	return view('socialmedia');
+}
+);
+Route::get('/edbackground',function()
+{
+	return view('edbackground');
+}
+);
